77d1db831c186ab9792e0192f478564ddc52f5ca
Levi~Ackerman <137004973+LeviAcker25@users.noreply.github.com>
2023-08-07 21:20:55 -0400

    Update core.c

--- Milestone 2/core.c
+++ Milestone 2/core.c
@@ -1,10 +1,7 @@
 /*
 *****************************************************************************
                         Assignment 1 - Milestone 2
-Full Name  : Kabir Narula
-Student ID#: 127962223
-Email      : Knarula9@myseneca.ca
-Section    : NAA
+
 Authenticity Declaration:
 I declare this submission is the result of my own work and has not been
 shared with any other student or 3rd party content provider. This submitted
